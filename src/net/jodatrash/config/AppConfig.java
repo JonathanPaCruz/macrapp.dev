29e3702e4d2638bfbd2188b00824ebf8b83d7250
Jonathan Pacheco <JonathanPachecoCruz@hotmail.com>
2025-06-19 18:45:37 -0600

    feat: add AppConfig.java

---
+++ src/net/jodatrash/config/AppConfig.java
@@ -0,0 +1,18 @@
+package net.jodatrash.config;
+
+import java.awt.*;
+
+public class AppConfig {
+  public static final int WIDTH = 48;
+  public static final Color BACKGROUND_COLOR = Color.BLACK;
+  public static final Color BORDER_COLOR = Color.GREEN;
+  public static final Color MENU_TEXT_COLOR = Color.GREEN;
+  public static final Color HOST_TEXT_COLOR = Color.RED;
+
+  public static final boolean BOLD_BORDER_CHAR = false;
+  public static final boolean BOLD_BORDER_SIDE = true;
+  public static final boolean BOLD_MENU_TEXT = false;
+
+  public static final char BORDER_CHAR = '=';
+  public static final String BORDER_SIDE = "*";
+}
